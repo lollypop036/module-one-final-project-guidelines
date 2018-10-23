73495bb16d16a830c8ce8c991ee1e4244ffa6ebc
ju-zp <jhuntington@tutanota.com>
2018-10-23 15:00:01 +0100

    added methods to the curator class and created main responsibilities

--- app/models/curator.rb
+++ app/models/curator.rb
@@ -4,4 +4,33 @@ class Curator <  ActiveRecord::Base
     has_many :artists, through: :collaborations
     has_many :bookings, through: :exhibitions
     has_many :users, through: :bookings
+
+    def create_exhibition(name, date, location, time)
+        Exhibition.create(name: name, date: date, location: location, time: time, curator_id: self.id)
+    end
+
+    def get_exhibitions
+        Exhibition.find_by(curator_id: self.id)
+    end
+
+    def search_for_collaborations
+        Collaboration.all
+    end
+
+    def assign_collaboration_to_exhibition(collaboration, exhibition)
+        collaboration.exhibition_id = exhibition.id
+    end
+
+    def assign_style_to_exhibition(style, exhibition)
+        exhibition.style_id = style.id
+    end
+
+    def self.search_credentials(username, password)
+        if self.find_by(username: username, password: password)
+            self.find_by(username: username, password: password)
+        else
+            false
+        end
+    end
+
 end
--- db/migrate/003_create_exhibitions.rb
+++ db/migrate/003_create_exhibitions.rb
@@ -3,13 +3,11 @@ class CreateExhibitions < ActiveRecord::Migration[5.0]
     def change
         create_table :exhibitions do |t|
             t.string :name
-            t.datetime :date
+            t.string :date
             t.string :location
-            t.datetime :time
+            t.string :time
             t.integer  :curator_id
             t.integer :style_id
-            t.integer :collaborator_id
-
         end
     end
 end
