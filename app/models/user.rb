d48b817077f4a24119faf01f59f7235fd60f8d11
github username <slonanuga@gmail.com>
2018-10-23 11:27:11 +0100

    rake migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User <  ActiveRecord::Base
     has_many :bookings
     has_many :exhibitions, through: :bookings
     has_many :collaborators, through: :exhibitions
-    has_many :artists, through :collaborators 
-    has_many :styles, through :artists
+    has_many :artists, through: :collaborators 
+    has_many :styles, through: :artists
 end
