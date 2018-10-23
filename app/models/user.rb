98ad959c7e10543447671748da4e84cb25cf4f03
github username <slonanuga@gmail.com>
2018-10-23 10:52:31 +0100

    user, exhibition and booking migrations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User <  ActiveRecord::Base
+    has_many :bookings
+    has_many :exhibitions, through: :bookings
+    has_many :collaborators, through: :exhibitions
+    has_many :artists, through :collaborators 
+    has_many :styles, through :artists
+end
