3e621a9da19fa526fd41a4f67c8ee86c8773e24c
github username <slonanuga@gmail.com>
2018-10-23 16:31:48 +0100

    user and artist class methods

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -7,4 +7,16 @@ class Artist < ActiveRecord::Base
     has_many :bookings, through: :exhibitions
     has_many :users, through: :bookings
 
+
+    def view_collaborations
+        Collaboration.where("artist_id == ?" self.id)
+    end
+
+    def view_exhibitions
+        User.get_exhibitions_by_collaborations(self)
+    end
+
+
+
+
 end
