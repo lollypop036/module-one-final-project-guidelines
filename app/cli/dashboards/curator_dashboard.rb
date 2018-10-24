a039b03454ecf89708bc5e20f85722712e81ba2d
ju-zp <jhuntington@tutanota.com>
2018-10-24 08:52:18 +0100

    added command_list method to output list of commands to user

--- app/cli/dashboards/curator_dashboard.rb
+++ app/cli/dashboards/curator_dashboard.rb
@@ -8,10 +8,20 @@ class CuratorDashboard
 
     def run
         welcome_message
+        command_list
     end
 
     def welcome_message
         puts "Welcome Curator #{curator.name}."
+    end
+
+    def command_list
+        puts "\nWhat would you like to do?"
+        puts "\n1. Edit profile."
+        puts "2. View your Exhibitions."
+        puts "3. Create a new Exhibition."
+        puts "4. Search Artists."
+        puts "5. Search Collaborations."
     end
 
 end
