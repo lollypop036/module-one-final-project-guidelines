c6986cdacdcf9660ea60c002b0c9e38820a2a5ea
ju-zp <jhuntington@tutanota.com>
2018-10-24 09:29:10 +0100

    added basic functionality for the edit menu in curator dashboard

--- app/cli/dashboards/curator/curator_edit_menu.rb
+++ app/cli/dashboards/curator/curator_edit_menu.rb
@@ -8,22 +8,48 @@ class CuratorEditMenu
 
     def run
         welcome_message
-        command_list
-        get_user_input
+        while(true)
+            command_list
+            command = get_user_input
+            if(command == "1")
+                change_name
+            elsif(command == "2")
+                change_username
+            elsif(command == "3")
+                change_password
+            elsif(command == "4")
+                break
+            else
+                puts "The number you entered did not relate to a command, try again."
+            end
+        end
+
+    end
+
+    def change_name
+        puts "\nChange Name"
+
+    end
+
+    def change_username
+    end
+
+    def change_password
     end
 
     def welcome_message
         puts "\nWelcome to the Edit menu."
+    end
 
     def command_list
-        puts "1. Change name."
+        puts "\n1. Change name."
         puts "2. Change Username."
         puts "3. Change Password."
         puts "4. Quit to main dashboard."
     end
 
     def get_user_input
-        puts "\nPlease enter a command number.(1-6)"
+        puts "\nPlease enter a command number.(1-4)"
         gets.chomp
     end
 
--- bin/run.rb
+++ bin/run.rb
@@ -3,8 +3,10 @@ require_relative '../config/environment'
 # ui = Interface.new
 # ui.run
 curator = Curator.all[0]
-cd = CuratorDashboard.new(curator)
-cd.run
+# cd = CuratorDashboard.new(curator)
+# cd.run
+edit_menu = CuratorEditMenu.new(curator)
+edit_menu.run
 
 # ruby bin/run.rb
 
