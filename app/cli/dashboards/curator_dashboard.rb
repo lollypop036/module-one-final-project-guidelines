375191ce169068042ea92520dea43cd863c06ac7
ju-zp <jhuntington@tutanota.com>
2018-10-24 09:01:02 +0100

    made and tested get_user_input method and built the while loop for the run method, basic dashboard functionality created

--- app/cli/dashboards/curator_dashboard.rb
+++ app/cli/dashboards/curator_dashboard.rb
@@ -8,7 +8,27 @@ class CuratorDashboard
 
     def run
         welcome_message
-        command_list
+        while(true)
+            command_list
+            command = get_user_input
+            #binding.pry
+            if(command == "1")
+                #edit profile
+            elsif(command == "2")
+                #view exhibitions
+            elsif(command == "3")
+                #create exhibition
+            elsif(command == "4")
+                #search artists
+            elsif(command == "5")
+                #search collabs
+            elsif(command == "6")
+                puts "Goodbye, Curator #{curator.name}"
+                break
+            else
+                puts "The number you entered did not relate to a command, try again."
+            end
+        end
     end
 
     def welcome_message
@@ -22,6 +42,12 @@ class CuratorDashboard
         puts "3. Create a new Exhibition."
         puts "4. Search Artists."
         puts "5. Search Collaborations."
+        puts "6. Sign Out."
+    end
+
+    def get_user_input
+        puts "\nPlease enter a command number.(1-6)"
+        gets.chomp
     end
 
 end
