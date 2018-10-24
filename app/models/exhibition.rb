07f1113bae68214b0428bba0553a1f2a6bcd9e4c
ju-zp <jhuntington@tutanota.com>
2018-10-24 15:55:21 +0100

    Finished user_exhibition_view_menu and tested. Updated the Exhibition class with class methods to retreive previous exhibitions and upcoming exhibitions.

--- app/models/exhibition.rb
+++ app/models/exhibition.rb
@@ -5,5 +5,13 @@ class Exhibition <  ActiveRecord::Base
     has_many :bookings
     has_many :users, through: :bookings
     has_many :artists, through: :collaborations
+
+    def self.view_previous_exhibitions(today_date)
+        self.all.select{|e| Date.parse(e.date) < today_date}
+    end
+
+    def self.view_upcoming_exhibitions(today_date)
+        self.all.select{|e| Date.parse(e.date) >= today_date}
+    end
     
 end
